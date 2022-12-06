ad3ebe0a4893d1f7600be390532305a973749aca
Sirinya <siri.richardson@gmail.com>
2022-12-06 23:44:29 +0000

    Add maps to each of the trail. Structure only

---
+++ assets/js/trail-maps.js
@@ -0,0 +1,243 @@
+(function () {
+     var bartliIcon = L.icon({
+         iconUrl: './assets/media/Character.png',
+
+         iconSize: [38, 95],
+         iconAnchor: [22, 94],
+         popupAnchor: [-3, -76]
+     });
+
+     var map = L.map('map-full').setView([46.94896343969532, 8.983088829099756], 15);
+
+     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         maxZoom: 19,
+         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+     }).addTo(map);
+
+     L.marker([46.956213832171656, 8.984484977695853], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.98470857410699, 9.016233614991968], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.95123611884839, 8.979083914633994], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.95034977358299, 8.98649435313304], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.95348709912568, 8.991837166896113], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.95146490276345, 8.994363450972257], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.939252143053196, 8.990858123322022], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.948288913753764, 8.988409406578468], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.94204159798816, 8.980649655468024], {
+         icon: bartliIcon
+     }).addTo(map);
+     L.marker([46.9416624091632, 8.98124906404019], {
+         icon: bartliIcon
+     }).addTo(map);
+     // L.marker([51.5, -0.09], {icon: bartliIcon}).addTo(map);
+
+    var map = L.map('map-variation-1').setView([46.94896343969532, 8.983088829099756], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.marker([46.956213832171656, 8.984484977695853], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.98470857410699, 9.016233614991968], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95123611884839, 8.979083914633994], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95034977358299, 8.98649435313304], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95348709912568, 8.991837166896113], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95146490276345, 8.994363450972257], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.939252143053196, 8.990858123322022], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.948288913753764, 8.988409406578468], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.94204159798816, 8.980649655468024], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.9416624091632, 8.98124906404019], {
+        icon: bartliIcon
+    }).addTo(map);
+    // L.marker([51.5, -0.09], {icon: bartliIcon}).addTo(map);
+
+    var map = L.map('map-variation-2').setView([46.94896343969532, 8.983088829099756], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.marker([46.956213832171656, 8.984484977695853], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.98470857410699, 9.016233614991968], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95123611884839, 8.979083914633994], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95034977358299, 8.98649435313304], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95348709912568, 8.991837166896113], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95146490276345, 8.994363450972257], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.939252143053196, 8.990858123322022], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.948288913753764, 8.988409406578468], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.94204159798816, 8.980649655468024], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.9416624091632, 8.98124906404019], {
+        icon: bartliIcon
+    }).addTo(map);
+    // L.marker([51.5, -0.09], {icon: bartliIcon}).addTo(map);
+
+    var map = L.map('map-variation-3').setView([46.94896343969532, 8.983088829099756], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.marker([46.956213832171656, 8.984484977695853], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.98470857410699, 9.016233614991968], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95123611884839, 8.979083914633994], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95034977358299, 8.98649435313304], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95348709912568, 8.991837166896113], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95146490276345, 8.994363450972257], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.939252143053196, 8.990858123322022], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.948288913753764, 8.988409406578468], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.94204159798816, 8.980649655468024], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.9416624091632, 8.98124906404019], {
+        icon: bartliIcon
+    }).addTo(map);
+    // L.marker([51.5, -0.09], {icon: bartliIcon}).addTo(map);
+
+    var map = L.map('map-variation-4').setView([46.94896343969532, 8.983088829099756], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.marker([46.956213832171656, 8.984484977695853], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.98470857410699, 9.016233614991968], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95123611884839, 8.979083914633994], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95034977358299, 8.98649435313304], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95348709912568, 8.991837166896113], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95146490276345, 8.994363450972257], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.939252143053196, 8.990858123322022], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.948288913753764, 8.988409406578468], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.94204159798816, 8.980649655468024], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.9416624091632, 8.98124906404019], {
+        icon: bartliIcon
+    }).addTo(map);
+    // L.marker([51.5, -0.09], {icon: bartliIcon}).addTo(map);
+
+    var map = L.map('map-variation-5').setView([46.94896343969532, 8.983088829099756], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.marker([46.956213832171656, 8.984484977695853], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.98470857410699, 9.016233614991968], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95123611884839, 8.979083914633994], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95034977358299, 8.98649435313304], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95348709912568, 8.991837166896113], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.95146490276345, 8.994363450972257], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.939252143053196, 8.990858123322022], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.948288913753764, 8.988409406578468], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.94204159798816, 8.980649655468024], {
+        icon: bartliIcon
+    }).addTo(map);
+    L.marker([46.9416624091632, 8.98124906404019], {
+        icon: bartliIcon
+    }).addTo(map);
+    // L.marker([51.5, -0.09], {icon: bartliIcon}).addTo(map);
+})();
