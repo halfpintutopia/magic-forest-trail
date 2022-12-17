36200ff2a8802fb03e47bdb318937cdf25ef46ed
Sirinya <siri.richardson@gmail.com>
2022-12-17 11:53:13 +0000

    Fix fixed header covering content when using top nav to scroll to content.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,25 +6,30 @@
     const CLASS = {
         ACTIVE: 'active',
         NAV_ITEM_TRAILS: '.nav-item-trails',
-        MENU_ITEMS: 'ul.menu li'
+        MENU_ITEMS: 'ul.menu li',
+        MAIN_NAVIGATION: '.main-navigation'
     }
 
     const EVENT = {
-        HASHCHANGE: 'hashchange'
+        HASHCHANGE: 'hashchange',
+        RESIZE: 'resize'
     }
 
     const htmlElementMap = {
         navItemTrails: '',
-        menuItems: ''
+        menuItems: '',
+        navigationHeight: ''
     }
 
     const initHTMLElements = () => {
         htmlElementMap.menuItems = document.querySelectorAll(CLASS.MENU_ITEMS)
         htmlElementMap.navItemTrails = document.querySelector(CLASS.NAV_ITEM_TRAILS)
+        htmlElementMap.mainNavigation = document.querySelector(CLASS.MAIN_NAVIGATION)
     }
 
     const initEvents = () => {
         window.addEventListener(EVENT.HASHCHANGE, toggleNavActiveCls)
+        window.addEventListener(EVENT.RESIZE, setScrollPadding)
     }
 
 
@@ -39,6 +44,13 @@
         }
     }
 
+    const setScrollPadding = () => {
+        const mainNavigationHeight = htmlElementMap.mainNavigation.offsetHeight
+        document.documentElement.style.setProperty('--scroll-padding', `${mainNavigationHeight}px`)
+    }
+
+
     initHTMLElements()
     initEvents()
+    setScrollPadding()
 })();
